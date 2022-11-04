4af4d50d2cd7cc57975c98a202759d23988df1f8
Amanda Feix <af006393@cfjl.com.br>
2022-11-03 22:07:07 -0300

    dia03nov

--- public/index.php
+++ public/index.php
@@ -2,6 +2,12 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-echo date('d/m/Y H:i:s');
-echo '<br>';
-echo FRONTEND_TITLE;
+use Petshop\Core\DB;
+
+//$estados = DB::select('select * from estados where uf = ?',
+//['RS']);
+//var_dump($estados);
+
+//echo date('d/m/Y H:i:s');
+//echo '<br>';
+//echo FRONTEND_TITLE;
