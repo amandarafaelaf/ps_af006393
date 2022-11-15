b017ba8efd635dea0b7907b37b4c4acf1a4043ae
Amanda Feix <af006393@cfjl.com.br>
2022-11-15 14:26:08 -0300

    Criada classe de cliente com validações

--- src/Model/Cliente.php
+++ src/Model/Cliente.php
@@ -1,30 +1,132 @@
 <?php
 
-// clientes
+namespace Petshop\Model;
 
-class Cliente
+use Petshop\Core\Attribute\Campo;
+use Petshop\Core\Attribute\Entidade;
+use Petshop\Core\DAO;
+use Petshop\Core\Exception;
+use Respect\Validation\Validator as v;
+
+#[Entidade(name: 'clientes')]
+
+class Cliente extends DAO
 {
-    // Cód. Cliente, pk, nn, auto
-    protected $idcliente;
+    #[Campo(label:'Cód. Cliente', nn:true, pk:true, auto:true)]
+    protected $idCliente;
 
-    // Tipo, nn
+    #[Campo(label:'Tipo (PF/PJ)', nn:true)]
     protected $tipo;
 
-    // CPF / CNPJ, nn
-    protected $cpfcnpj;
+    #[Campo(label:'Documento', nn:true)]
+    protected $cpfCnpj;
 
-    // Nome, nn
+    #[Campo(label:'Nome Completo', nn:true)]
     protected $nome;
 
-    // Email, nn
+    #[Campo(label:'E-mail', nn:true)]
     protected $email;
 
-    // Senha, nn
+    #[Campo(label:'Senha', nn:true)]
     protected $senha;
     
-    // Dt. Criação, nn, auto
+    #[Campo(label:'Dt. Criação', nn:true, auto:true)]
     protected $created_at;
 
-    // Dt. Alteração, nn, auto
+    #[Campo(label:'Dt. Alteração', nn:true, auto:true)]
     protected $updated_at;
+
+    public function getIdCliente()
+    {
+        return $this->idCliente;
+    }
+
+    public function getTipo()
+    {
+        return $this->tipo;
+    }
+    public function setTipo(string $tipo): self
+    {
+        $tipo = strtoupper(trim($tipo));
+        if (!in_array($tipo, ['F', 'J'])) {
+            throw new Exception('O tipo de pessoa não está definido corretamente (F/J)');
+        }
+
+        $this->tipo = $tipo;
+        return $this;
+    }
+
+    public function getCpfCnpj()
+    {
+        return $this->cpfCnpj;
+    }
+    public function setCpfCnpj(string $cpfCnpj): self
+    {
+        if (in_array($this->tipo, ['F', 'J'])) {
+            throw new Exception('O tipo de pessoa precisa ser definido antes do documento (F/J)');
+        }
+
+        if ($this->tipo == 'F') {
+            $docValido = v::cpf()->validate($cpfCnpj);
+        } else {
+            $docValido = v::cnpj()->validate($cpfCnpj);
+        }
+
+        if (!$docValido) {
+            throw new Exception('O documento informado é inválido');
+        }
+
+        $this->cpfCnpj = $cpfCnpj;
+        return $this;
+    }
+
+    public function getNome()
+    {
+        return $this->nome;
+    }
+    public function setNome(string $nome): self
+    {
+        $this->nome = $nome;
+        return $this;
+    }
+
+    public function getEmail()
+    {
+        return $this->email;
+    }
+    public function setEmail(string $email): self
+    {
+        $email = strtolower(trim($email));
+
+        $emailValido = v::email()->validate($email);
+        if (!$emailValido) {
+            throw new Exception('O email informado é inválido');
+        }
+
+        $this->email = $email;
+        return $this;
+    }
+
+    public function getSenha()
+    {
+        return $this->senha;
+    }
+    public function setSenha(string $senha): self
+    {
+        $hashDaSenha = hash_hmac('md5', $senha, SALT_SENHA);
+        $senha = password_hash($hashDaSenha, PASSWORD_DEFAULT);
+        
+        $this->senha = $senha;
+        return $this;
+    }
+
+    public function getCreated_At()
+    {
+        return $this->created_at;
+    }
+
+    public function getUpdated_At()
+    {
+        return $this->updated_at;
+    }
 }
