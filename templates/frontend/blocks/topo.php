4de9a9448fc9389d19cfbdaabbfc490d0d272412
Amanda Feix <af006393@cfjl.com.br>
2022-11-16 21:45:50 -0300

    Teste de topo

--- templates/frontend/blocks/topo.php
+++ templates/frontend/blocks/topo.php
@@ -1,3 +1,24 @@
-<hr>
-este é o conteúdo do topo
-<hr>
+<nav class="navbar">
+    <div class="container-fluid">
+        <form class="d-flex col-md-12" role="search">
+            <button class="btn btn-outline-success me-2 col-md-2" type="submit">LOGO</button>
+            <input class="form-control me-2 md-7" type="search" placeholder="Busca" aria-label="Search">
+            <button class="btn btn-outline-success me-2 col-md-2" type="submit">Login/cadastro</button>
+            <button class="btn btn-outline-success me-2 col-md-1" type="submit"><i class="bi bi-heart-fill"></i></button>
+        </form>
+    </div>
+</nav>
+
+<div class="container-fluid">
+    <button class="btn btn-secondary dropdown-toggle col-md-2" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+        Departamentos
+    </button>
+    <ul class="dropdown-menu">
+        <li><a class="dropdown-item" href="#">Action</a></li>
+        <li><a class="dropdown-item" href="#">Another action</a></li>
+        <li><a class="dropdown-item" href="#">Something else here</a></li>
+    </ul>
+    <button class="btn btn-outline-success me-2 col-md-2" type="submit">Nossas lojas</button>
+    <button class="btn btn-outline-success md-2 col-md-2" type="submit">Fale conosco</button>
+    <button class="btn btn-outline-success me-2 col-md-2" type="submit">Fone: 55 5555-1111</button>
+</div>
