7f1284b59e5f99244d3a4bb8482d27fb4a82f64b
Amanda Feix <af006393@cfjl.com.br>
2022-11-17 21:44:07 -0300

    Criado o topo do front-end

--- templates/frontend/blocks/topo.php
+++ templates/frontend/blocks/topo.php
@@ -1,24 +1,75 @@
-<nav class="navbar">
-    <div class="container-fluid">
-        <form class="d-flex col-md-12" role="search">
-            <button class="btn btn-outline-success me-2 col-md-2" type="submit">LOGO</button>
-            <input class="form-control me-2 md-7" type="search" placeholder="Busca" aria-label="Search">
-            <button class="btn btn-outline-success me-2 col-md-2" type="submit">Login/cadastro</button>
-            <button class="btn btn-outline-success me-2 col-md-1" type="submit"><i class="bi bi-heart-fill"></i></button>
-        </form>
-    </div>
-</nav>
+<!-- hack para o topo fixo não "comer" o conteúdo da página -->
+<div style="margin-top: 5.5em">&nbsp;</div>
 
-<div class="container-fluid">
-    <button class="btn btn-secondary dropdown-toggle col-md-2" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-        Departamentos
-    </button>
-    <ul class="dropdown-menu">
-        <li><a class="dropdown-item" href="#">Action</a></li>
-        <li><a class="dropdown-item" href="#">Another action</a></li>
-        <li><a class="dropdown-item" href="#">Something else here</a></li>
-    </ul>
-    <button class="btn btn-outline-success me-2 col-md-2" type="submit">Nossas lojas</button>
-    <button class="btn btn-outline-success md-2 col-md-2" type="submit">Fale conosco</button>
-    <button class="btn btn-outline-success me-2 col-md-2" type="submit">Fone: 55 5555-1111</button>
+<div class="topo-site fixed-top">
+    <div class="container">
+        <div class="topo-site-superior row pt-3 pb-1">
+            <div class="col-2 topo-site-logo d-flex align-items-center">
+                <a href="/" title="Voltar ao início do site">
+                    <img src="https://picsum.photos/180/50" width="180" height="50" alt="Logo" class="img-fluid rounded-1">
+                </a>
+            </div>
+            <div class="topo-site-busca col-6">
+                <form action="/busca" method="GET" class="position-relative h-100 d-flex align-items-center">
+                    <input type="text" name="ps-busca" class="form-control input-busca rounded-5 pe-5">
+                    <button type="submit" class="btn-busca"><i class="bi bi-search"></i></button>
+                </form>
+            </div>
+            <div class="topo-site-opcoes col-4 row text-center align-items-center">
+                <div class="col-8 topo-site-opcoes-usr">
+                    <a href="/login" title="Entrar/Cadastrar" class="d-flex align-items-center">
+                        <i class="bi bi-person fs-3 pe-2"></i>
+                        <span>Entre ou cadastre-se</span>
+                    </a>
+                </div>
+                <div class="col-4 d-flex justify-content-between">
+                    <a href="/favoritos" title="Ver meus favoritos" class="px-3">
+                        <i class="bi bi-box2-heart fs-3"></i>
+                    </a>
+                    <a href="/carrinho" title="Ver meu carrinho" class="px-3">
+                        <i class="bi bi-cart3 fs-3"></i>
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="row topo-site-inferior">
+            <div class="topo-site-inferior-menu col-2">
+                <a class="d-flex align-items-center" data-bs-toggle="offcanvas" href="#offcanvas-menu" aria-controls="offcanvas-menu">
+                    <i class="bi bi-list fs-3 pe-1"></i>
+                    <span>Departamentos</span>
+                </a>
+            </div>
+            <div class="topo-site-inferior-contatos col-6 row">
+                <div class="col-auto d-flex align-items-center">
+                    <a href="/nossas-lojas" title="Conheça as nossas lojas">
+                        <i class="bi bi-geo-alt pe-1"></i>
+                        <span>Nossas lojas</span>
+                    </a>
+                </div>
+                <div class="col-auto d-flex align-items-center">
+                    <a href="/fale-conosco">
+                        <i class="bi bi-megaphone pe-1"></i>
+                        <span>Fale conosco</span>
+                    </a>
+                </div>
+            </div>
+            <div class="topo-site-inferior-fone col-4 justify-content-end d-flex align-items-center">
+                <i class="bi bi-telephone pe-2"></i>
+                <span>(11) 3303-9911</span>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<div class="offcanvas offcanvas-start rounded-3 m-3" data-bs-scroll="true" tabindex="-1" id="offcanvas-menu" aria-labelledby="offcanvas-menuLabel">
+    <div class="offcanvas-header">
+        <h5 class="offcanvas-title" id="offcanvas-menuLabel">Categorias do site</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+    </div>
+    <div class="offcanvas-body">
+        <p>Lista das categorias ex01</p>
+        <p>Lista das categorias ex02</p>
+        <p>Lista das categorias ex03</p>
+    </div>
 </div>
