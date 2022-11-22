f8c9b8c87da57723ec5c766cffcb036d8a490279
Amanda Feix <af006393@cfjl.com.br>
2022-11-22 19:48:25 -0300

    Criada estrutura para cadastro de cliente

--- src/Controller/CadastroController.php
+++ src/Controller/CadastroController.php
@@ -20,6 +20,25 @@ class CadastroController extends FrontController
 
     private function formCadastro()
     {
-        
+        $dados = [
+            'btn_label'=>'Criar minha conta',
+            'btn_class'=>'btn btn-success mt-5',
+            'fields'=>[
+                ['type'=>'radio-inline', 'class'=>'col-6', 'label'=>'Você é pessoa:', 'name'=>'tipo', 
+                    'options'=>[
+                        ['label'=>'Física', 'value'=>'F'],
+                        ['label'=>'Jurídica', 'value'=>'J']
+                    ], 
+                    'required'=>true
+                ],
+                ['type'=>'text', 'class'=>'col-6', 'label'=>'Documento', 'name'=>'cpfcnpj', 'required'=>true],
+                ['type'=>'text', 'label'=>'Nome completo', 'name'=>'nome', 'required'=>true],
+                ['type'=>'email', 'label'=>'E-mail', 'name'=>'email', 'required'=>true],
+                ['type'=>'password', 'label'=>'Crie uma senha', 'name'=>'senha', 'required'=>true],
+                ['type'=>'password', 'label'=>'Confirme sua senha', 'name'=>'senha2', 'required'=>true],
+            ]
+        ];
+
+        return Render::block('form', $dados);
     }
 }
