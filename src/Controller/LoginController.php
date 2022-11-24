a468a5db7ee8fba0bf445b6d0790d7721df98762
Amanda Feix <af006393@cfjl.com.br>
2022-11-24 19:47:02 -0300

    Criado processo de validação de usuario e senha

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -2,7 +2,9 @@
 
 namespace Petshop\Controller;
 
+use Exception;
 use Petshop\Core\FrontController;
+use Petshop\Model\Cliente;
 use Petshop\View\Render;
 
 class LoginController extends FrontController
@@ -16,6 +18,42 @@ class LoginController extends FrontController
         $dados['formLogin'] = $this->formLogin();
 
         Render::front('login', $dados);
+    }
+    public function postLogin()
+    {
+        try {
+            if (empty($_POST['email'] || empty($_POST['senha']))) {
+                throw new Exception('Os campos de usuário e senha devem ser informados');
+            }
+    
+            if (strlen($_POST['senha']) < 5) {
+                throw new Exception('Senha muito curta, digite ao menos 5 caracteres');
+            }
+    
+            $dadosUsuario = (new Cliente)->find(['email ='=>$_POST['email']]);
+    
+            if (!count($dadosUsuario)) {
+                throw new Exception('Usuário ou senha inválidos');
+            }
+    
+            $hashDaSenha = hash_hmac('md5', $_POST['senha'], SALT_SENHA);
+            $senhaNoBanco = $dadosUsuario[0]['senha'];
+            $senhaValida = password_verify($hashDaSenha, $senhaNoBanco);
+    
+            if (!$senhaValida) {
+                throw new Exception('Usuário ou senha inválidos');
+            }
+    
+            $_SESSION['cliente'] = $dadosUsuario[0];
+            redireciona('/meus-dados');
+        } catch(Exception $e) {
+            $_SESSION['mensagem'] = [
+                'tipo'  =>  'warning',
+                'texto' =>  $e->getMessage()
+            ];
+            $_POST['senha'] = '';
+            $this->login();
+        }
     }
 
     private function formLogin()
